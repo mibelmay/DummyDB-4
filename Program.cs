11eaed076b0dbeae7a4a52d7c116c0778aa9c81d
Stacy <zemcovannastasia11@gmail.com>
2023-03-20 00:38:46 +0500

    working

--- Program.cs
+++ Program.cs
@@ -1,13 +1,57 @@
 ﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace DummyDB
 {
     public class Program
     { 
-        public static void Main(string[] args) 
+        public static void Main(string[] args)
         {
-
-
+            TableScheme scheme = TableScheme.ReadFile(".\\book.json");
+            Console.WriteLine(scheme.Name);
+            Console.WriteLine();
         }
     }
+
+    class TableScheme
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("columns")]
+        public List<Column> Columns { get; }
+
+        public static TableScheme ReadFile(string path)
+        {
+            return JsonSerializer.Deserialize<TableScheme>(File.ReadAllText(path));
+        }
+    }
+
+    class Column
+    { 
+        public string Name { get; }
+        public ColumnType Type { get; }
+    }
+
+    class Table
+    {
+        private TableScheme Scheme { get; }
+
+    }
+
+    class Row
+    {
+        public Dictionary<Column, object> Data { get; }
+    }
+
+
+    enum ColumnType
+    {
+        String,
+        Int,
+        Uint,
+        Double,
+        DateTime
+    }
+
 }
