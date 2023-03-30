05028067e16a7e07af80ceab48f696f409992101
Stacy <zemcovannastasia11@gmail.com>
2023-03-31 00:01:56 +0500

    working

--- Program.cs
+++ Program.cs
@@ -8,11 +8,14 @@ namespace DummyDB
         public static void Main(string[] args)
         {
             TableScheme scheme = TableScheme.ReadFile(".\\book.json");
-            foreach (Column column in scheme.Columns) {
-                Console.WriteLine(column.Name);
-                Console.WriteLine(column.Type);
-                Console.WriteLine();
-            }
+            //foreach (Column column in scheme.Columns) {
+            //    Console.WriteLine(column.Name);
+            //    Console.WriteLine(column.Type);
+            //    Console.WriteLine();
+            //}
+            Table table = ReadTable.Read(scheme, "\\books.csv");
+            Console.WriteLine(table.Rows[0].Data.Keys);
+            
         }
     }
 
@@ -22,32 +25,21 @@ namespace DummyDB
         public string Name { get; set; }
         [JsonPropertyName("type")]
         public string Type { get; set; }
-
+       
     }
 
     class Table
     {
-        private TableScheme Scheme { get; }
+        public List<Row> Rows { get; set; }
     }
 
     class Row
     {
-        public Dictionary<Column, object> Data { get; }
-    }
-
-
-    enum ColumnType
-    {
-        String,
-        Int,
-        Uint,
-        Double,
-        DateTime
-    }
-
-    class ReadTable
-    {
-
+        public Dictionary<Column, object> Data { get; set; }
+        public Row() 
+        {
+            Data = new Dictionary<Column, object>();
+        }
     }
 
 }
---
+++ ReadTable.cs
@@ -0,0 +1,103 @@
+﻿namespace DummyDB
+{
+    //enum ColumnType
+    //{
+    //    String,
+    //    Int,
+    //    Uint,
+    //    Double,
+    //    DateTime
+    //}
+
+    class ReadTable
+    {
+        public static Table Read(TableScheme scheme, string path)
+        {
+            string[] data = File.ReadAllLines(Directory.GetCurrentDirectory() + path);
+            Table table = new Table();
+            table.Rows = new List<Row>();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                string[] line = data[i].Split(';');
+
+                if(line.Length != scheme.Columns.Count)
+                {
+                    throw new Exception($"В строке {i + 1} неверное количество столбцов");
+                }
+
+                table.Rows.Add(AddRow(scheme, line, i));
+            }
+            
+            return table;
+        }
+
+        public static Row AddRow(TableScheme scheme, string[] line, int i)
+        {   
+            Row row = new Row();
+            for (int j = 0; j < line.Length; j++)
+            {
+                switch (scheme.Columns[j].Type)
+                {
+                    case ("int"):
+                        {
+                            if (int.TryParse(line[j], out int data))
+                            {
+                                row.Data.Add(scheme.Columns[j], data);
+                            }
+                            else
+                            {
+                                throw new Exception($"В сроке {i + 1} в столбце {j+1} указаны некорректные данные");
+                            }
+                            break;
+                        }
+                    case ("uint"):
+                        {
+                            if (uint.TryParse(line[j], out uint data))
+                            {
+                                row.Data.Add(scheme.Columns[j], data);
+                            }
+                            else
+                            {
+                                throw new Exception($"В сроке {i + 1} в столбце {j + 1} указаны некорректные данные");
+                            }
+                            break;
+                        }
+                    case ("datetime"):
+                        {
+                            if (DateTime.TryParse(line[j], out DateTime data))
+                            {
+                                row.Data.Add(scheme.Columns[j], data);
+                            }
+                            else
+                            {
+                                throw new Exception($"В сроке {i + 1} в столбце {j + 1} указаны некорректные данные");
+                            }
+                            break;
+                        }
+                    case ("double"):
+                        {
+                            if (double.TryParse(line[j], out double data))
+                            {
+                                row.Data.Add(scheme.Columns[j], data);
+                            }
+                            else
+                            {
+                                throw new Exception($"В сроке {i + 1} в столбце {j + 1} указаны некорректные данные");
+                            }
+                            break;
+                        }
+                    default:
+                        row.Data.Add(scheme.Columns[j], line[j]);
+                        break;
+                }
+
+            }
+
+            return row;
+        }
+
+    }
+
+
+}
